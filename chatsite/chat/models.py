61e1bb7fccaeeae72ae492d2fe1b875904f6a0bb
Сергей Токмаков <s.tokmakov@ermolinskie.com>
2023-10-06 15:20:02 +0300

    new5

--- chatsite/chat/models.py
+++ chatsite/chat/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.urls import reverse
 
 class Room(models.Model):
     name = models.CharField(max_length=128)
@@ -31,3 +33,32 @@ class Message(models.Model):
         return f'{self.user.username}: {self.content} [{self.timestamp}]'
 
 
+class Profile(models.Model):
+    """Модель профиля пользователя"""
+    user = models.OneToOneField(User, verbose_name="Пользователь", on_delete=models.CASCADE)
+    avatar = models.ImageField("Аватар", upload_to="profile/", blank=True, null=True)
+    email_two = models.EmailField("Доп. email")
+    phone = models.CharField("Телефон", max_length=25)
+    first_name = models.CharField("Имя", max_length=50)
+    last_name = models.CharField("Фамилия", max_length=50, blank=True, null=True)
+    slug = models.SlugField("URL", max_length=50, default='')
+    #TODO: Адрес, гео база
+
+    def __str__(self):
+        return self.first_name
+
+    class Meta:
+        verbose_name = "Профиль"
+        verbose_name_plural = "Профиля"
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    """Создание профиля пользователя при регистрации"""
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
